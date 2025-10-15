49fc463af433b9855f137a0a94712c9a845783b0
dsitant voices <distant_voices1996@outlook.com>
2025-10-15 16:56:44 +0800

    希望你能继续活下去

---
+++ keep_on_living.rb
@@ -0,0 +1,29 @@
+# Copyright (c) 2025 DistantVoices
+# =============================功能说明=============================
+# sketchup插件：活下去！
+# 督促你活下去
+# 每30分钟提醒你提肛
+# 每90分钟提醒你站立
+# 每120分钟提醒你喝水
+# 每180分钟提醒你休息
+# 增加一个小说阅读器
+# 每天需要打卡，如果超过3天没打卡，自动联系紧急联系人，说你加班猝死了
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module DistantVoices
+  module Keep_On_Living
+
+    unless file_loaded?(__FILE__)
+      ex = SketchupExtension.new('活下去！', 'Keep_On_Living/main')
+      ex.description = '设计行业的加班情况日益严重，希望通过这个工具来提醒你活下去'
+      ex.version     = "0.1.0"
+      ex.copyright   = 'Copyright (c) 2025 DistantVoices.'
+      ex.creator     = 'DistantVoices'
+      Sketchup.register_extension(ex, true)
+      file_loaded(__FILE__)
+    end
+
+  end
+end
